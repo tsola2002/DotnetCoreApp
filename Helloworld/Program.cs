eb7a7ff4b3c5b6026e8b9e47ab10e9783cb4016c
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2019-11-26 05:39:38 +0100

    created dotnet console application

---
+++ Helloworld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating a variable
+            var a = "Hello Omatsola";
+
+            //prints out text to the console screen
+            Console.WriteLine(a);
+        }
+    }
+}
---
+++ configure_input/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace configure_input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this code takes in an input
+            var a = Console.ReadLine();
+
+            //prints out text to the console screen
+            Console.WriteLine(a);
+        }
+    }
+}
