477778bc58dee9b1cb70eca5e9297fcc1f2e0284
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2019-11-28 05:15:05 +0100

    reading & printing out information with readline & writeline

---
+++ multiple-choice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace multiple_choice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what is your name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("what is age?");
+            var age = Console.ReadLine();
+
+            Console.WriteLine("what is birth month?");
+            var month = Console.ReadLine();
+
+            // we use a string formatter to output the name
+            Console.WriteLine("Your name is {0}", name);
+            Console.WriteLine("Your age is {0}", age);
+            Console.WriteLine("Your birth month is {0}", month);
+        }
+    }
+}
