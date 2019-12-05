b7ffe4c8aa97b5e6c3d2730dabf1d112aec03e36
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2019-12-05 06:14:59 +0100

    using for loop for better efficiency

---
+++ for-loop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace for_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var count = 1;
+
+            // while (count <= 10)
+            // {
+            //     Console.WriteLine(count);
+            //     count++;
+            // }
+
+
+            // REVAMPED VERSION USING FOR LOOP
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
