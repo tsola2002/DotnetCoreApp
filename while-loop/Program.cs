f44fd92fa968a5fc0c26f22e19a4e6a134316a2b
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2019-12-05 06:02:12 +0100

    used while loops in the program

---
+++ while-loop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace paascode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var passcode = "";
+
+            //creating while loop
+            while(passcode != "secret")
+            {
+                Console.WriteLine("what is your password?");
+                passcode = Console.ReadLine();
+
+                if(passcode != "secret")
+                {
+                    Console.WriteLine("Unauthorized Access");
+                }
+            }
+            // end of while loop
+            Console.WriteLine("You Have Been Authenticated");
+            
+        }
+    }
+}
