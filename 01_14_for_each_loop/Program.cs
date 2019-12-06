823b45210a21b38bffbc0fd33430e6681d251724
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2019-12-06 05:43:59 +0100

    using the foreach loop to iterate over the array

---
+++ 01_14_for_each_loop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+ 
+namespace _01_13_school_tracker
+{
+    class Program
+    {
+        // the benefit of using an array is being able to iterate through its values using a loop
+        static void Main(string[] args)
+        {
+         
+               var studentGrades = new int[] { 80, 77, 45, 87, 53, 43, 67, 24, 86, 98, 40 }; 
+
+                //we output studentgrade inside our student grades array
+                foreach( var studentGrade in studentGrades)
+                {
+                    Console.WriteLine(studentGrade);
+                }
+
+            Console.WriteLine(studentGrades[4]);
+        }
+    }
+}
