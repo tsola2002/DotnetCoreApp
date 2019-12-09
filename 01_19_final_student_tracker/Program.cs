f08b8b4325540e87cf4f398bd33fd6e0c75e4ad0
Omatsola Isaac Sobotie <omatsola.sobotie@fintraksoftware.com>
2019-12-09 19:52:23 +0100

    solved the challenge

---
+++ 01_19_final_student_tracker/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+// PROBLEM: THE SCHOOL WANTS TO BE ABLE TO TRACK 10 STUDENTS INSIDE A CLASS & THEIR GRADES
+// SOLUTION: 
+namespace _01_19_final_student_tracker
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            Console.WriteLine("How many students in your class?");
+            var studentCount = int.Parse(Console.ReadLine());
+
+        
+            var studentNames = new string[studentCount]; 
+            var studentGrades = new int[studentCount]; 
+
+            // we will use a for loop that will count through the student count
+            for(int i = 0; i < studentCount; i++)
+            {
+                Console.WriteLine("Student Name: ");
+                studentNames[i] = Console.ReadLine();
+
+                Console.WriteLine("Student Grade: ");
+                studentGrades[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < studentCount; i++)
+            {
+                Console.WriteLine("Name: {0}, Grade: {1}", studentNames[i], studentGrades[i]);
+            }   
+            
+        }
+    }
+}
