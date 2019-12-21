5e119ab9fb0b65d136c12a746d94a0cdb5b78670
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2019-12-21 16:11:40 +0100

    debugging using breakpoints

---
+++ 03_04_debugging_using_breakpoints/Console.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+// renaming your namespace avoids conflicts with other namespaces
+namespace Util
+{
+    class Console
+    {
+        // creating the console function which will ask the questions
+       static public string Ask(string question)
+       {
+           System.Console.Write(question);
+           return System.Console.ReadLine();   
+       }
+       static public int AskInt(string question)
+       {
+           try
+           {
+               System.Console.Write(question);
+               return int.Parse(System.Console.ReadLine());
+           }
+           catch (Exception)
+           {
+               //we provide a custom message as a parameter
+               throw new FormatException("Input was not a number");
+           }
+       } 
+    }
+}
