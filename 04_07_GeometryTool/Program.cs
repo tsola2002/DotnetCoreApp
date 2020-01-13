1efd2ca860fc42eaea326ba91a7cace55b3b7853
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2020-01-13 12:03:51 +0100

    using abstract classes in c#

---
+++ 04_07_GeometryTool/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _04_07_GeometryTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // we instantiate the square with an instantiated width property of 2 
+            var square = new Square() { Width = 2 };
+            // we instantiate the triangle with an instantiated base property of 2 & height of 5
+            var triangle = new Triangle() { Base = 2, Height = 5 }; 
+
+            // we both square & triangle methods(OLD)
+            //Console.WriteLine(square.GetArea());
+            //Console.WriteLine(triangle.GetArea());
+
+            // we both square & triangle methods(NEW)
+            square.Display();
+            triangle.Display();
+        }
+    }
+
+
+    abstract class Shape
+    {
+        public abstract int GetArea();
+
+        public void Display()
+        {
+            Console.WriteLine("The area is {0}", GetArea());        
+        } 
+         
+    }
+
+    class Square : Shape
+    {
+        public int Width;
+
+        // we unify the function with the abstract class function above
+        public override int GetArea()
+        {
+            return Width * Width;
+        }
+    }
+
+    class Triangle : Shape
+    {
+        public int Base;
+        public int Height;
+
+        // we unify the function with the abstract class function above
+        public override int GetArea()
+        {
+            return (Base * Height) / 2;
+        }
+    }
+}
